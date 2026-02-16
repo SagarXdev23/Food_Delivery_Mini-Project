58463ced363d7ec55570b1a2882a8796a37b0181
shourav <shourav185.com>
2026-02-16 22:52:02 +0530

    editing

---
+++ frontend/food/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Navbar from './components/navbar/navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Cart from './pages/Home/Cart/Cart'
+import PlaceOrder from './pages/Home/Cart/PlaceOrder/PlaceOrder'
+import Header from './components/navbar/Header/Header'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbar />
+       
+      {/* <Routes>
+       
+        <Route path='/' element= {<Home/>} />
+        <Route path='/Cart' element= {<Cart/>} />
+        <Route path='/order' element= {<PlaceOrder />} />
+      </Routes> */}
+    </div>
+  )
+}
+
+export default App
+
---
+++ frontend/food/src/components/navbar/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Choose from a diverse menu featurinf a delectable array of dishes crafted with the finest ingrediants and culinary expertise. our mission is to satisfy your craving your dining experience , one dekicioys meal at a time.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
