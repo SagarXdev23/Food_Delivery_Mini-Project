a7ba474009e6cb8a3814a309bde7db6beca4c9a7
SagarXdev23 <sagarmishra1113@gmail.com>
2026-02-19 15:01:58 +0530

    Changes in Header

--- frontend/food/src/components/navbar/Header/Header.jsx
+++ frontend/food/src/components/navbar/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <div className="header">
       <div className="header-contents">
         <h2>Order your favourite food here</h2>
-        <p>Choose from a diverse menu featurinf a delectable array of dishes crafted with the finest ingrediants and culinary expertise. our mission is to satisfy your craving your dining experience , one dekicioys meal at a time.</p>
+         <p>Discover an expansive menu crafted to delight every palate — from comforting, time-honored classics and vibrant international plates to seasonal chef specials. Each dish is prepared with fresh, high-quality ingredients and thoughtful technique. Click "View Menu" to explore categories, customize your order, and enjoy fast delivery or easy pickup.</p>
         <button>View Menu</button>
       </div>
     </div>
