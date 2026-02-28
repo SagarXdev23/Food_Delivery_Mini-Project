cc288452e20368a34653bbba0ef2022745a8ab70
shourav <shourav185.com>
2026-02-28 14:02:16 +0530

    added more interface

--- frontend/food/src/assets/assets.js
+++ frontend/food/src/assets/assets.js
@@ -1,3 +1,131 @@
+// import logo from './logo.png'
+// import add_icon from './add_icon.png'
+// import order_icon from './order_icon.png'
+// import profile_image from './profile_image.png'
+// import upload_area from './upload_area.png'
+// import parcel_icon from './parcel_icon.png'
+// import search_icon from './search_icon.png'
+// import basket_icon from './basket_icon.png'
+// import menu_1 from './menu_1.png'
+// import menu_2 from './menu_2.png'
+// import menu_3 from './menu_3.png'
+// import menu_4 from './menu_4.png'
+// import menu_5 from './menu_5.png'
+// import menu_6 from './menu_6.png'
+// import menu_7 from './menu_7.png'
+// import menu_8 from './menu_8.png'
+
+// import food_1 from './food_1.png'
+
+
+// export const assets ={
+//     logo,
+//     add_icon,
+//     order_icon,
+//     profile_image,
+//     upload_area,
+//     parcel_icon,
+//     search_icon,
+//     basket_icon
+// }
+
+// export const menu_list = [
+//     {
+//         menu_name : "Salad",
+//         menu_image : menu_1
+//     },
+//     {
+//         menu_name : "Rolls",
+//         menu_image : menu_2
+//     },
+//     {
+//         menu_name : "Desets",
+//         menu_image : menu_3
+//     },
+//     {
+//         menu_name : "Rolls",
+//         menu_image : menu_4
+//     },
+//     {
+//         menu_name : "Desets",
+//         menu_image : menu_5
+//     },
+//     {
+//         menu_name : "Rolls",
+//         menu_image : menu_6
+//     },
+//     {
+//         menu_name : "Desets",
+//         menu_image : menu_7
+//     },
+//     {
+//         menu_name : "Rolls",
+//         menu_image : menu_8
+//     }
+// ]
+
+// export const food_list = [
+//     {
+//         _id : "1",
+//         name: "Greek salad",
+//         image: food_1,
+//         price: 12,
+//         description:"Foood provied nutreints for overall health and well being ",
+//         category : "Salad"
+//     },
+//     {
+//         _id : "1",
+//         name: "Greek salad",
+//         image: food_1,
+//         price: 12,
+//         description:"Foood provied nutreints for overall health and well being ",
+//         category : "Salad"
+//     },
+//     {
+//         _id : "1",
+//         name: "Greek salad",
+//         image: food_1,
+//         price: 12,
+//         description:"Foood provied nutreints for overall health and well being ",
+//         category : "Salad"
+//     },
+//     {
+//         _id : "1",
+//         name: "Greek salad",
+//         image: food_1,
+//         price: 12,
+//         description:"Foood provied nutreints for overall health and well being ",
+//         category : "Salad"
+//     },
+//     {
+//         _id : "1",
+//         name: "Greek salad",
+//         image: food_1,
+//         price: 12,
+//         description:"Foood provied nutreints for overall health and well being ",
+//         category : "Salad"
+//     },
+//     {
+//         _id : "1",
+//         name: "Greek salad",
+//         image: food_1,
+//         price: 12,
+//         description:"Foood provied nutreints for overall health and well being ",
+//         category : "Salad"
+//     }
+// ]
+
+// export const url = 'http://localhost:4000'
+
+
+
+
+
+
+
+
+
+
 import logo from './logo.png'
 import add_icon from './add_icon.png'
 import order_icon from './order_icon.png'
@@ -6,6 +134,14 @@ import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 import search_icon from './search_icon.png'
 import basket_icon from './basket_icon.png'
+import facebook_icon from './facebook_icon.png'
+import linkedin_icon from './linkedin_icon.png'
+import twitter_icon from './twitter_icon.png'
+import play_store from './play_store.png'
+import app_store from './app_store.png'
+
+
+
 import menu_1 from './menu_1.png'
 import menu_2 from './menu_2.png'
 import menu_3 from './menu_3.png'
@@ -17,8 +153,7 @@ import menu_8 from './menu_8.png'
 
 import food_1 from './food_1.png'
 
-
-export const assets ={
+export const assets = {
     logo,
     add_icon,
     order_icon,
@@ -26,94 +161,72 @@ export const assets ={
     upload_area,
     parcel_icon,
     search_icon,
-    basket_icon
+    basket_icon,
+    play_store,
+    app_store,
+    facebook_icon,
+    twitter_icon,
+    linkedin_icon
 }
 
 export const menu_list = [
-    {
-        menu_name : "Salad",
-        menu_image : menu_1
-    },
-    {
-        menu_name : "Rolls",
-        menu_image : menu_2
-    },
-    {
-        menu_name : "Desets",
-        menu_image : menu_3
-    },
-    {
-        menu_name : "Rolls",
-        menu_image : menu_4
-    },
-    {
-        menu_name : "Desets",
-        menu_image : menu_5
-    },
-    {
-        menu_name : "Rolls",
-        menu_image : menu_6
-    },
-    {
-        menu_name : "Desets",
-        menu_image : menu_7
-    },
-    {
-        menu_name : "Rolls",
-        menu_image : menu_8
-    }
+    { menu_name: "Salad", menu_image: menu_1 },
+    { menu_name: "Rolls", menu_image: menu_2 },
+    { menu_name: "Desserts", menu_image: menu_3 }, // Corrected spelling
+    { menu_name: "Sandwich", menu_image: menu_4 }, // Changed from redundant 'Rolls'
+    { menu_name: "Cake", menu_image: menu_5 },     // Changed from redundant 'Desets'
+    { menu_name: "Pure Veg", menu_image: menu_6 }, 
+    { menu_name: "Pasta", menu_image: menu_7 }, 
+    { menu_name: "Noodles", menu_image: menu_8 }
 ]
 
 export const food_list = [
     {
-        _id : "1",
+        _id: "1",
         name: "Greek salad",
         image: food_1,
         price: 12,
-        description:"Foood provied nutreints for overall health and well being ",
-        category : "Salad"
+        description: "Food provides nutrients for overall health and well-being",
+        category: "Salad"
     },
     {
-        _id : "1",
-        name: "Greek salad",
+        _id: "2", // Unique ID
+        name: "Veg salad",
         image: food_1,
-        price: 12,
-        description:"Foood provied nutreints for overall health and well being ",
-        category : "Salad"
+        price: 18,
+        description: "Food provides nutrients for overall health and well-being",
+        category: "Salad"
     },
     {
-        _id : "1",
-        name: "Greek salad",
+        _id: "3", // Unique ID
+        name: "Clover Salad",
         image: food_1,
-        price: 12,
-        description:"Foood provied nutreints for overall health and well being ",
-        category : "Salad"
+        price: 16,
+        description: "Food provides nutrients for overall health and well-being",
+        category: "Salad"
     },
     {
-        _id : "1",
-        name: "Greek salad",
+        _id: "4", // Unique ID
+        name: "Chicken Salad",
         image: food_1,
-        price: 12,
-        description:"Foood provied nutreints for overall health and well being ",
-        category : "Salad"
+        price: 24,
+        description: "Food provides nutrients for overall health and well-being",
+        category: "Salad"
     },
     {
-        _id : "1",
-        name: "Greek salad",
+        _id: "5", // Unique ID
+        name: "Lasagna Rolls",
         image: food_1,
-        price: 12,
-        description:"Foood provied nutreints for overall health and well being ",
-        category : "Salad"
+        price: 14,
+        description: "Food provides nutrients for overall health and well-being",
+        category: "Rolls"
     },
     {
-        _id : "1",
-        name: "Greek salad",
+        _id: "6", // Unique ID
+        name: "Peri Peri Rolls",
         image: food_1,
         price: 12,
-        description:"Foood provied nutreints for overall health and well being ",
-        category : "Salad"
+        description: "Food provides nutrients for overall health and well-being",
+        category: "Rolls"
     }
 ]
-
-// export const url = 'http://localhost:4000'
-
