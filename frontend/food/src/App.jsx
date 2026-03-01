17cb9760fb916594cb4cb085bb022a77d7ef9d58
shourav <shourav185.com>
2026-03-01 12:55:29 +0530

    added signup and login

--- frontend/food/src/App.jsx
+++ frontend/food/src/App.jsx
@@ -1,4 +1,45 @@
-import React from 'react'
+// import React from 'react'
+// import Navbar from './components/navbar/navbar'
+// import { Route, Routes } from 'react-router-dom'
+// import Home from './pages/Home/Home'
+// import Cart from './pages/Home/Cart/Cart'
+// import PlaceOrder from './pages/Home/Cart/PlaceOrder/PlaceOrder'
+// import Header from './components/navbar/Header/Header';
+// import FoodDisplay from './components/navbar/FoodDisplay/FoodDisplay'
+// import Footer from './components/navbar/Footer/Footer'
+
+
+// const App = () => {
+//   return (
+// <>
+// <div className='app'>
+//       <Navbar />
+     
+       
+//       <Routes>
+       
+//         <Route path='/' element= {<Home/>} />
+//         <Route path='/Cart' element= {<Cart/>} />
+//         <Route path='/order' element= {<PlaceOrder />} />
+//       </Routes>
+//  </div>
+//  <Footer/>
+// </>
+    
+   
+//   )
+// }
+
+// export default App
+
+
+
+
+
+
+
+import React, { useState } from 'react'
+
 import Navbar from './components/navbar/navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
@@ -7,13 +48,18 @@ import PlaceOrder from './pages/Home/Cart/PlaceOrder/PlaceOrder'
 import Header from './components/navbar/Header/Header';
 import FoodDisplay from './components/navbar/FoodDisplay/FoodDisplay'
 import Footer from './components/navbar/Footer/Footer'
+import LoginPopup from './components/navbar/LoginPopup/LoginPopup'
 
 
 const App = () => {
+
+  const [showLogin,setShowLogin] = useState(false)
+
   return (
 <>
+   {showLogin ? <LoginPopup setShowLogin={setShowLogin} />:<></>}
 <div className='app'>
-      <Navbar />
+      <Navbar setShowLogin ={setShowLogin}/>
      
        
       <Routes>
@@ -26,15 +72,8 @@ const App = () => {
  <Footer/>
 </>
     
-    
-   
-
-
-
-
    
   )
 }
 
 export default App
-
