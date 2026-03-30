c53fbcd925fbe5ba72c5d35d3966f9edd92812cd
SagarXdev23 <sagarmishra1113@gmail.com>
2026-03-30 19:42:34 +0530

    added env file

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";    
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://sagarmishragla_db_user:50536802@cluster0.vg3ypz3.mongodb.net/Food-Delivery').then(() =>
-        console.log('MongoDB Connected'));
+    await mongoose.connect(process.env.MONGO_URI)
+        console.log('MongoDB Connected');
 } 
