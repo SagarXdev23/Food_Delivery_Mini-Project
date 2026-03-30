58811c20e7980a316bd6b27357a0d35a45b554fc
SagarXdev23 <sagarmishra1113@gmail.com>
2026-03-30 20:16:45 +0530

    fixed some errors

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";    
 export const connectDB = async () => {
-    await mongoose.connect(process.env.MONGO_URI)
+    await mongoose.connect('mongodb+srv://sagarmishragla_db_user:50536802@cluster0.vg3ypz3.mongodb.net/Food-Delivery').then(() => {
         console.log('MongoDB Connected');
-} 
+    })
+}
